5e5e27b319e8a032a8d154840b2018889a74d0fb
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-05-12 12:33:42 -0700

    more work on header and styling

--- pages/profile.js
+++ pages/profile.js
@@ -10,16 +10,17 @@ import EditButton from "../components/edit-profile-button";
 
 export default function Profile() {
     return (
-        <div className="mx-8">
+        <div className="">
 
             <Header title="Talk Dev To Me" username="ashcas" />
-            <div className="md:ml-96 lg:mx-auto xl:mx-auto md:w-96 lg:w-96 xl:w-96 md:-mt-30 lg:-mt-50 xl:-mt-30">
-                <div className="z-1"><img src={"/static/images/abstract-bg-image-blue.jpeg"} alt="custom image" className="w-full object-top h-24 rounded-lg md:h-48 lg:h-48 xl:h-48"/></div>
-                <div className="z-100 -mt-20 md:-mt-40 lg:-mt-40 xl:-mt-40"><img src={"/static/images/profile-pic.png"} alt="my picture" className="mx-auto h-32 rounded-full md:h-48  lg:h-48  xl:h-48"/></div>
-            </div>
+            {/* <div className="md:ml-96 lg:mx-auto xl:mx-auto md:h-96 lg:h-96 xl:h-96 md:-mt-30 lg:-mt-50 xl:-mt-50"> */}
+                <div className="md:ml-96 lg:mx-auto xl:mx-auto md:h-96 lg:h-96 xl:h-96 md:-mt-30 lg:-mt-50 xl:-mt-50"><img src={"/static/images/pythonista.png"} alt="custom image" className="mx-auto h-32 rounded-full md:h-48  lg:h-48  xl:h-48"/></div>
+                {/* <div className="z-1 invisible"><img src={"/static/images/abstract-bg-image-blue.jpeg"} alt="custom image" className="w-full object-top h-24 rounded-lg md:h-48 lg:h-48 xl:h-48"/></div> */}
+                {/* <div className="z-100 -mt-20 md:-mt-40 lg:-mt-40 xl:-mt-40"><img src={"/static/images/profile-pic.png"} alt="my picture" className="mx-auto h-32 rounded-full md:h-48  lg:h-48  xl:h-48"/></div> */}
+            {/* </div> */}
             {/* Main Page Div */}
             <div >
-                <div className="flex justify-between text-left mx-4 md:mx-8 lg:mx-8 xl:mx-8">
+                <div className="flex justify-between text-left mx-8 md:mx-16 lg:mx-16 xl:mx-16">
                     <div className="md:-mt-32 lg:-mt-32 xl:-mt-32"><Languages /></div>
                     <EditButton />
                     <div className="md:-mt-32 lg:-mt-32 xl:-mt-32"><Links /></div>
